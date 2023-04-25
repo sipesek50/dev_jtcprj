982a503a315d10589e93158324ee7abb406f41c5
Audy <audywijaya50@gmail.com>
2023-04-25 19:56:05 +0700

    Setting env

--- models/async.js
+++ models/async.js
@@ -1,9 +1,12 @@
+require('dotenv').config()
+
 async function async_query(query, data) {
     const mysql = require("mysql2/promise")
     const conn = await mysql.createConnection({
-        host: 'localhost',
-        user: 'root',
-        database: 'dev_jtcprj'
+        host: process.env.DATABASE_HOST,
+        user: process.env.DATABASE_USERNAME,
+        password: process.env.DATABASE_PASSWORD,
+        database: process.env.DATABSE_DATABASE
     })
     const [rows, fields] = await conn.execute(query, data)
     return await [rows, fields]
